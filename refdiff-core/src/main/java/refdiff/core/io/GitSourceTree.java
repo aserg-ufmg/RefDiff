cb853277d2156ed2276b71a3d33abb44a6177ce9
Danilo Ferreira e Silva <danilo.ferreira@almg.gov.br>
2018-06-08 18:51:54 -0300

    Read git tree

---
+++ refdiff-core/src/main/java/refdiff/core/io/GitSourceTree.java
@@ -0,0 +1,44 @@
+package refdiff.core.io;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectReader;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.treewalk.TreeWalk;
+
+public class GitSourceTree extends SourceFileSet {
+	
+	private final Repository repo;
+	private final ObjectId sha1;
+	
+	public GitSourceTree(Repository repo, ObjectId commitSha1, List<SourceFile> sourceFiles) {
+		super(sourceFiles);
+		this.repo = repo;
+		this.sha1 = commitSha1;
+	}
+	
+	@Override
+	public String readContent(SourceFile sourceFile) throws IOException {
+		try (ObjectReader reader = repo.newObjectReader(); RevWalk walk = new RevWalk(reader)) {
+			RevCommit commit = walk.parseCommit(sha1);
+			
+			RevTree tree = commit.getTree();
+			TreeWalk treewalk = TreeWalk.forPath(reader, sourceFile.getPath(), tree);
+			
+			if (treewalk != null) {
+				byte[] data = reader.open(treewalk.getObjectId(0)).getBytes();
+				return new String(data, StandardCharsets.UTF_8.name());
+			} else {
+				throw new FileNotFoundException(sourceFile.getPath());
+			}
+		}
+	}
+	
+}
