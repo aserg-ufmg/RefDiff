f34599bd46b2f8abd9b2fa7ec741d47fc1d34a01
João Paulo Ribeiro da Silva <joaop.ribs@gmail.com>
2018-06-11 20:01:51 -0300

    Precision calculation adjustments and latest results

--- refdiff-evaluation/src/test/java/refdiff/evaluation/c/PrecisionCalculation.java
+++ refdiff-evaluation/src/test/java/refdiff/evaluation/c/PrecisionCalculation.java
@@ -11,16 +11,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 import javax.json.JsonObject;
 
 import org.apache.commons.lang3.StringEscapeUtils;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.lib.RepositoryBuilder;
 import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.revwalk.RevWalk;
-import org.eclipse.jgit.revwalk.filter.RevFilter;
 
 import com.jcabi.github.Coordinates;
 import com.jcabi.github.Github;
@@ -32,8 +30,10 @@ import refdiff.core.diff.RastComparator;
 import refdiff.core.diff.RastDiff;
 import refdiff.core.diff.Relationship;
 import refdiff.core.diff.RelationshipType;
+import refdiff.core.io.GitHelper;
+import refdiff.core.io.SourceFileSet;
 import refdiff.core.rast.RastNode;
-import refdiff.evaluation.EvaluationUtils;
+import refdiff.core.util.PairBeforeAfter;
 import refdiff.evaluation.ExternalProcess;
 import refdiff.parsers.c.CParser;
 
@@ -206,13 +206,15 @@ public class PrecisionCalculation {
 	}
 	
 	private static void findRefactoringsOnRepository(String repositoryName, String repositoryUrl, int repositoryOrder, int totalRepositories) {
+		long timeBefore = System.currentTimeMillis();
+		
 		String tempFolder = System.getProperty("java.io.tmpdir");
 		String repoFolderPath = tempFolder + repositoryName + ".git";
 		File repoFolder = new File(tempFolder, repositoryName + ".git");
 		
 		if (!repoFolder.exists()) {
 			System.out.println("Cloning " + repositoryUrl + " into " + repoFolderPath);
-			ExternalProcess.execute(new File(tempFolder), "git", "clone", repositoryUrl, "--bare", "--shallow-since=2017-01-01");
+			ExternalProcess.execute(new File(tempFolder), "git", "clone", repositoryUrl, repoFolder.getPath(), "--bare", "--depth=1000");
 		}
 		
 		String fileName = RESULTS_DIRECTORY + File.separator + repositoryName + ".csv";
@@ -220,27 +222,26 @@ public class PrecisionCalculation {
 		System.out.println(repositoryOrder + "/" + totalRepositories + 
 				" Analysing " + repositoryName + " and printing refactorings to " + fileName);
 		
-		int commitsCount = 0;
+		AtomicInteger commitsCount = new AtomicInteger(0);
 		Map<String, Integer> relationshipCounts = new HashMap<String, Integer>();
+		
+		GitHelper gh = new GitHelper();
 		
 		try (
 			PrintWriter writer = new PrintWriter(new File(fileName));
-			Repository repository = new RepositoryBuilder()
-				.setGitDir(repoFolder)
-				.readEnvironment()
-				.build();
-			RevWalk revWalk = new RevWalk(repository)) {
-			
-			RevCommit head = revWalk.parseCommit(repository.resolve("HEAD"));
-			revWalk.markStart(head);
-			revWalk.setRevFilter(RevFilter.NO_MERGES);
-			
-			for (RevCommit commit : revWalk) {
-				System.out.println("Commit " + (commitsCount + 1) + "/" + NUMBER_OF_COMMITS_TO_CONSIDER);
+			Repository repository = gh.openRepository(repoFolder);) 
+		{
+			gh.forEachNonMergeCommit(repository, NUMBER_OF_COMMITS_TO_CONSIDER, (RevCommit commitBefore, RevCommit commitAfter) -> {
+				String sha = commitAfter.getName();
 				
-				String sha = commit.getName();
+				System.out.println("Commit " + (commitsCount.intValue() + 1) + "/" + NUMBER_OF_COMMITS_TO_CONSIDER + " - " + sha);
 				
-				Set<Relationship> relationships = getRelationships(tempFolder, repoFolderPath, sha);
+				Set<Relationship> relationships;
+				try {
+					relationships = getRelationships(gh, repository, commitBefore, commitAfter);
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
 				
 				Map<String, Integer> thisCommitsRelationshipCounts = new HashMap<String, Integer>();
 			    
@@ -256,7 +257,7 @@ public class PrecisionCalculation {
     					csvField(relationshipString), 
     					csvField(before), 
     					csvField(after), 
-    					csvField(commit.getShortMessage()), 
+    					csvField(commitAfter.getShortMessage()), 
     					Double.toString(Math.random())
 	    			};
 	    			
@@ -285,18 +286,17 @@ public class PrecisionCalculation {
 			    
 			    System.out.println(" (total " + countTotal + ")");
 			    
-			    commitsCount++;
-			    if (commitsCount == NUMBER_OF_COMMITS_TO_CONSIDER) {
-			    	break;
-			    }
-			}
+			    commitsCount.incrementAndGet();
+			});
 		} 
 		catch (Exception e) {
 			e.printStackTrace();
 			return;
 		}
 		finally {
-			System.out.print("Done printing " + fileName + ". " + commitsCount + " commits considered");
+			long timeAfter = System.currentTimeMillis();
+			
+			System.out.print("Done printing " + fileName + ". " + commitsCount.get() + " commits considered");
 			
 			String separator = ". ";
 			int countRelationships = 0;
@@ -311,8 +311,8 @@ public class PrecisionCalculation {
 				separator = "; ";
 			}
 			
-			System.out.print(". Total relationships found " + countRelationships + ".");
-			System.out.println();
+			System.out.println(". Total relationships found " + countRelationships + ".");
+			System.out.println("Took " + ((timeAfter - timeBefore) / 1000) + " seconds");
 		}
 	}
 	
@@ -333,12 +333,14 @@ public class PrecisionCalculation {
 		counters.put(relationship, counter + 1);
 	}
 	
-	private static Set<Relationship> getRelationships(String tempFolder, String repositoryFolder, String sha) throws Exception {
+	private static Set<Relationship> getRelationships(GitHelper gh, Repository repository, RevCommit commitBefore, RevCommit commitAfter) 
+			throws Exception {
 		CParser parser = new CParser();
 		RastComparator rastComparator = new RastComparator(parser, parser);
-		EvaluationUtils evaluationUtils = new EvaluationUtils(rastComparator, tempFolder);
 		
-		RastDiff diff = evaluationUtils.computeDiff(repositoryFolder, sha);
+		PairBeforeAfter<SourceFileSet> sources = gh.getSourcesBeforeAndAfterCommit(
+				repository, commitBefore, commitAfter, parser.getAllowedFileExtensions());
+		RastDiff diff = rastComparator.compare(sources.getBefore(), sources.getAfter());
 		
 		Set<Relationship> relationships = diff.getRelationships().stream()
 				.filter(relationship -> !relationship.getType().equals(RelationshipType.SAME))
