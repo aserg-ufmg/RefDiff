515a3beaada5b0fd1a71845ff2b255fc721c0089
João Paulo Ribeiro da Silva <joaop.ribs@gmail.com>
2018-06-20 18:04:49 -0300

    Class to list all refactorings on a single commit

---
+++ refdiff-evaluation/src/test/java/refdiff/evaluation/c/ListRefactoringsOnCommit.java
@@ -0,0 +1,70 @@
+package refdiff.evaluation.c;
+
+import java.io.File;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.eclipse.jgit.lib.Repository;
+
+import refdiff.core.diff.RastComparator;
+import refdiff.core.diff.RastDiff;
+import refdiff.core.diff.Relationship;
+import refdiff.core.diff.RelationshipType;
+import refdiff.core.io.GitHelper;
+import refdiff.core.io.SourceFileSet;
+import refdiff.core.util.PairBeforeAfter;
+import refdiff.evaluation.ExternalProcess;
+import refdiff.parsers.c.CParser;
+
+public class ListRefactoringsOnCommit {
+	
+	public static void main(String[] args) throws Exception {
+		if (args.length != 2) {
+			System.err.println("You need enter params <Repository Full Name> <Commit SHA>");
+			return;
+		}
+		
+		String repositoryFullName = args[0];
+		String commitSHA = args[1];
+		
+		String[] repositoryNameParts = repositoryFullName.split("/");
+		
+		String repositoryOwner = repositoryNameParts[0];
+		String repositoryName = repositoryNameParts[1];
+		
+		String repositoryUrl = "https://github.com/" + repositoryOwner + "/" + repositoryName + ".git";
+		
+		String tempFolder = System.getProperty("java.io.tmpdir");
+		String repoFolderPath = tempFolder + repositoryName + ".git";
+		File repoFolder = new File(tempFolder, repositoryName + ".git");
+		
+		if (!repoFolder.exists()) {
+			System.out.println("Cloning " + repositoryUrl + " into " + repoFolderPath);
+			ExternalProcess.execute(new File(tempFolder), "git", "clone", repositoryUrl, repoFolder.getPath(), "--bare", "--depth=1000");
+//			ExternalProcess.execute(new File(tempFolder), "git", "clone", repositoryUrl, repoFolder.getPath(), "--bare", "--shallow-since=2007-01-01");
+		}
+		
+		CParser parser = new CParser();
+		RastComparator rastComparator = new RastComparator(parser, parser);
+		GitHelper gh = new GitHelper();
+		
+		try (Repository repo = gh.openRepository(repoFolder)) {
+			
+			PairBeforeAfter<SourceFileSet> sources = gh.getSourcesBeforeAndAfterCommit(
+					repo, commitSHA, parser.getAllowedFileExtensions());
+			RastDiff diff = rastComparator.compare(sources.getBefore(), sources.getAfter());
+			
+			Set<Relationship> relationships = diff.getRelationships().stream()
+				.filter(relationship -> !relationship.getType().equals(RelationshipType.SAME))
+				.collect(Collectors.toSet());
+			
+			System.out.println(relationships.size() + " relationships detected");
+			
+			relationships.stream()
+				.forEach(relationship -> {
+					System.out.println(relationship.toString());
+				});
+		}
+	}
+	
+}
