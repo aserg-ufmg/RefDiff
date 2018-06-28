d1febbbb9583b2a41b33e8692c89da3312fa4bce
Danilo F. Silva <danilofes@gmail.com>
2018-06-27 23:46:42 -0300

    Babel parser

---
+++ refdiff-js/src/main/java/refdiff/parsers/js/BabelParser.java
@@ -0,0 +1,179 @@
+package refdiff.parsers.js;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+import jdk.nashorn.api.scripting.ScriptObjectMirror;
+import refdiff.core.io.FilePathFilter;
+import refdiff.core.io.SourceFile;
+import refdiff.core.io.SourceFileSet;
+import refdiff.core.rast.HasChildrenNodes;
+import refdiff.core.rast.Location;
+import refdiff.core.rast.RastNode;
+import refdiff.core.rast.RastNodeRelationship;
+import refdiff.core.rast.RastNodeRelationshipType;
+import refdiff.core.rast.RastRoot;
+import refdiff.parsers.RastParser;
+import refdiff.parsers.SourceTokenizer;
+
+public class BabelParser implements RastParser, SourceTokenizer {
+	
+	private Invocable invocableScript;
+	private int nodeCounter = 0;
+	
+	public BabelParser() throws Exception {
+		ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+		
+		engine.eval("load('classpath:jvm-npm.js');");
+		engine.eval("var babelParser = require('@babel/parser');");
+		engine.eval("function parse(script) {return babelParser.parse(script, {ranges: true, sourceType: 'unambiguous'});}");
+		engine.eval("function tokenize(source) {return babelParser.parse(source, {tokens: true}).tokens;}");
+		engine.eval("function toJson(object) {return JSON.stringify(object);}");
+		this.invocableScript = (Invocable) engine;
+	}
+	
+	@Override
+	public RastRoot parse(SourceFileSet sources) throws Exception {
+		RastRoot root = new RastRoot();
+		this.nodeCounter = 0;
+		for (SourceFile sourceFile : sources.getSourceFiles()) {
+			String content = sources.readContent(sourceFile);
+			getRast(root, sourceFile, content, sources);
+		}
+		return root;
+	}
+	
+	@Override
+	public List<String> tokenize(String source) {
+		try {
+			ScriptObjectMirror array = (ScriptObjectMirror) this.invocableScript.invokeFunction("tokenize", source);
+			List<String> tokens = new ArrayList<>(array.size());
+			for (int i = 0; i < array.size(); i++) {
+				String token = ((ScriptObjectMirror) array.getSlot(i)).getMember("value").toString();
+				tokens.add(token);
+			}
+			return tokens;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	private void getRast(RastRoot root, SourceFile sourceFile, String content, SourceFileSet sources) throws Exception {
+		try {
+//			System.out.print(String.format("Parsing %s ... ", sources.describeLocation(sourceFile)));
+//			long timestamp = System.currentTimeMillis();
+			ScriptObjectMirror esprimaAst = (ScriptObjectMirror) this.invocableScript.invokeFunction("parse", content);
+//			System.out.println(String.format("Done in %d ms", System.currentTimeMillis() - timestamp));
+			JsValue astRoot = new JsValue(esprimaAst, this::toJson);
+			Map<String, Object> callerMap = new HashMap<>();
+			getRast(0, root, sourceFile, astRoot, callerMap);
+			root.forEachNode((calleeNode, depth) -> {
+				if (calleeNode.getType().equals("FunctionDeclaration") && callerMap.containsKey(calleeNode.getLocalName())) {
+					RastNode callerNode = (RastNode) callerMap.get(calleeNode.getLocalName());
+					root.getRelationships().add(new RastNodeRelationship(RastNodeRelationshipType.USE, callerNode.getId(), calleeNode.getId()));
+				}
+			});
+		} catch (ScriptException e) {
+			throw new RuntimeException(String.format("Error parsing %s: %s", sources.describeLocation(sourceFile), e.getMessage()));
+		}
+	}
+	
+	private void getRast(int depth, HasChildrenNodes container, SourceFile sourceFile, JsValue esprimaAst, Map<String, Object> callerMap) throws Exception {
+		if (!esprimaAst.has("type")) {
+			throw new RuntimeException("object is not an AST node");
+		}
+		String path = sourceFile.getPath();
+		String type = esprimaAst.get("type").asString();
+		JsValue range = esprimaAst.get("range");
+		int begin = range.get(0).asInt();
+		int end = range.get(1).asInt();
+		int bodyBegin = begin;
+		int bodyEnd = end;
+		if (esprimaAst.has("body")) {
+			JsValue body = esprimaAst.get("body");
+			if (body.has("range")) {
+				JsValue bodyRange = body.get("range");
+				bodyBegin = bodyRange.get(0).asInt();
+				bodyEnd = bodyRange.get(1).asInt();
+				if (body.get("type").asString().equals("BlockStatement")) {
+					bodyBegin = bodyBegin + 1;
+					bodyEnd = bodyEnd - 1;
+				}
+			}
+		}
+		
+		if (EsprimaNodeHandler.RAST_NODE_HANDLERS.containsKey(type)) {
+			EsprimaNodeHandler handler = EsprimaNodeHandler.RAST_NODE_HANDLERS.get(type);
+			RastNode rastNode = new RastNode(++nodeCounter);
+			rastNode.setType(type);
+			rastNode.setLocation(new Location(path, begin, end, bodyBegin, bodyEnd));
+			rastNode.setLocalName(handler.getLocalName(rastNode, esprimaAst));
+			rastNode.setSimpleName(handler.getSimpleName(rastNode, esprimaAst));
+			rastNode.setNamespace(handler.getNamespace(rastNode, esprimaAst));
+			rastNode.setStereotypes(handler.getStereotypes(rastNode, esprimaAst));
+			rastNode.setParameters(handler.getParameters(rastNode, esprimaAst));
+			container.addNode(rastNode);
+			container = rastNode;
+		} else {
+			if ("CallExpression".equals(type)) {
+				extractCalleeNameFromCallExpression(esprimaAst, callerMap, container);
+			}
+		}
+		
+		for (String key : esprimaAst.getOwnKeys()) {
+			JsValue value = esprimaAst.get(key);
+			if (value.isObject()) {
+				if (value.has("type")) {
+					getRast(depth + 1, container, sourceFile, value, callerMap);
+				}
+			}
+			if (value.isArray()) {
+				for (int i = 0; i < value.size(); i++) {
+					JsValue element = value.get(i);
+					if (element.has("type")) {
+						getRast(depth + 1, container, sourceFile, element, callerMap);
+					}
+				}
+			}
+		}
+	}
+
+	private void extractCalleeNameFromCallExpression(JsValue callExpresionNode, Map<String, Object> callerMap, HasChildrenNodes container) {
+		JsValue callee = callExpresionNode.get("callee");
+		if (callee.get("type").asString().equals("MemberExpression")) {
+			JsValue property = callee.get("property");
+			if (property.get("type").asString().equals("Identifier")) {
+				String calleeName = property.get("name").asString();
+				callerMap.put(calleeName, container);
+			} else {
+				// callee is a complex expression, not an identifier
+			}
+		} else if (callee.get("type").asString().equals("Identifier")) {
+			String calleeName = callee.get("name").asString();
+			callerMap.put(calleeName, container);
+		} else {
+			// callee is a complex expression, not an identifier
+		}
+	}
+	
+	private String toJson(Object object) {
+		try {
+			return this.invocableScript.invokeFunction("toJson", object).toString();
+		} catch (NoSuchMethodException | ScriptException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	@Override
+	public FilePathFilter getAllowedFilesFilter() {
+		return new FilePathFilter(Arrays.asList(".js", ".jsx"), Arrays.asList(".min.js"));
+	}
+}
---
+++ refdiff-js/src/test/java/refdiff/parsers/js/TestJsParserBabel.java
@@ -0,0 +1,134 @@
+package refdiff.parsers.js;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Set;
+
+import org.junit.Test;
+
+import refdiff.core.io.SourceFile;
+import refdiff.core.io.SourceFolder;
+import refdiff.core.rast.Location;
+import refdiff.core.rast.RastNode;
+import refdiff.core.rast.RastNodeRelationship;
+import refdiff.core.rast.RastNodeRelationshipType;
+import refdiff.core.rast.RastRoot;
+import refdiff.test.util.JsParserSingletonBabel;
+
+public class TestJsParserBabel {
+	
+	private BabelParser parser = JsParserSingletonBabel.get();
+	
+	@Test
+	public void shouldParseSimpleFile() throws Exception {
+		Path basePath = Paths.get("test-data/parser/js/");
+		SourceFolder sources = SourceFolder.from(basePath, Paths.get("ex1.js"));
+		RastRoot root = parser.parse(sources);
+		
+		assertThat(root.getNodes().size(), is(1));
+		
+		RastNode nodeScriptEx1 = root.getNodes().get(0);
+		assertThat(nodeScriptEx1.getType(), is("Program"));
+		assertThat(nodeScriptEx1.getNamespace(), is(""));
+		assertThat(nodeScriptEx1.getLocation(), is(new Location("ex1.js", 0, 71)));
+		
+		assertThat(nodeScriptEx1.getNodes().size(), is(2));
+		RastNode nodeArrowFn = nodeScriptEx1.getNodes().get(0);
+		RastNode nodeFnHello = nodeScriptEx1.getNodes().get(1);
+		
+		assertThat(nodeArrowFn.getType(), is("ArrowFunctionExpression"));
+		assertThat(nodeArrowFn.getLocation(), is(new Location("ex1.js", 16, 23, 22, 23)));
+		assertThat(nodeArrowFn.getLocalName(), is(""));
+		
+		assertThat(nodeFnHello.getType(), is("FunctionDeclaration"));
+		assertThat(nodeFnHello.getLocation(), is(new Location("ex1.js", 28, 71, 46, 70)));
+		assertThat(nodeFnHello.getLocalName(), is("hello"));
+		assertThat(nodeFnHello.getParameters().size(), is(0));
+	}
+	
+	@Test
+	public void shouldParseFunctionCall() throws Exception {
+		Path basePath = Paths.get("test-data/parser/js/");
+		SourceFolder sources = SourceFolder.from(basePath, Paths.get("ex2.js"));
+		RastRoot root = parser.parse(sources);
+		
+		assertThat(root.getNodes().size(), is(1));
+		
+		RastNode nodeScriptEx2 = root.getNodes().get(0);
+		assertThat(nodeScriptEx2.getType(), is("Program"));
+		
+		assertThat(nodeScriptEx2.getNodes().size(), is(2));
+		RastNode nodeF1 = nodeScriptEx2.getNodes().get(0);
+		RastNode nodeF2 = nodeScriptEx2.getNodes().get(1);
+		
+		assertThat(nodeF1.getType(), is("FunctionDeclaration"));
+		assertThat(nodeF1.getLocalName(), is("f1"));
+		
+		assertThat(nodeF2.getType(), is("FunctionDeclaration"));
+		assertThat(nodeF2.getLocalName(), is("f2"));
+		
+		Set<RastNodeRelationship> relationships = root.getRelationships();
+		assertThat(relationships.size(), is(1));
+		assertThat(relationships, hasItem(rel(RastNodeRelationshipType.USE, nodeF1, nodeF2)));
+	}
+	
+	@Test
+	public void shouldParseClassDeclaration() throws Exception {
+		Path basePath = Paths.get("test-data/parser/js/");
+		SourceFolder sources = SourceFolder.from(basePath, Paths.get("ex3.js"));
+		RastRoot root = parser.parse(sources);
+		
+		assertThat(root.getNodes().size(), is(1));
+		
+		RastNode nodeScriptEx3 = root.getNodes().get(0);
+		assertThat(nodeScriptEx3.getType(), is("Program"));
+		
+		assertThat(nodeScriptEx3.getNodes().size(), is(1));
+		RastNode nodeRectangle = nodeScriptEx3.getNodes().get(0);
+		
+		assertThat(nodeRectangle.getType(), is("ClassDeclaration"));
+		assertThat(nodeRectangle.getLocalName(), is("Rectangle"));
+		
+		assertThat(nodeRectangle.getNodes().size(), is(3));
+		
+		RastNode contructor = nodeRectangle.getNodes().get(0);
+		assertThat(contructor.getType(), is("MethodDefinition"));
+		assertThat(contructor.getLocalName(), is("constructor"));
+		assertThat(contructor.getParameters().size(), is(2));
+		assertThat(contructor.getParameters().get(0).getName(), is("height"));
+		assertThat(contructor.getParameters().get(1).getName(), is("width"));
+		
+		RastNode methodGetArea = nodeRectangle.getNodes().get(1);
+		assertThat(methodGetArea.getType(), is("MethodDefinition"));
+		assertThat(methodGetArea.getLocalName(), is("area"));
+		
+		RastNode methodCalcArea = nodeRectangle.getNodes().get(2);
+		assertThat(methodCalcArea.getType(), is("MethodDefinition"));
+		assertThat(methodCalcArea.getLocalName(), is("calcArea"));
+	}
+	
+	@Test
+	public void shouldParseLargeFile() throws Exception {
+		Path basePath = Paths.get("test-data/parser/js/");
+		SourceFolder sources = SourceFolder.from(basePath, Paths.get("input.js"));
+		RastRoot root = parser.parse(sources);
+		
+		assertThat(root.getNodes().size(), is(1));
+	}
+	
+	@Test
+	public void shouldTokenizeLargeFile() throws Exception {
+		Path basePath = Paths.get("test-data/parser/js/");
+		SourceFolder sources = SourceFolder.from(basePath, Paths.get("input.js"));
+		for (SourceFile file : sources.getSourceFiles()) {
+			parser.tokenize(sources.readContent(file));
+		}
+	}
+	
+	private RastNodeRelationship rel(RastNodeRelationshipType type, RastNode n1, RastNode n2) {
+		return new RastNodeRelationship(type, n1.getId(), n2.getId());
+	}
+}
