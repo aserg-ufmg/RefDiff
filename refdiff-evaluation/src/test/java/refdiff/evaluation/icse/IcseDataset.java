124bb269172061c4e1d86be8c79073e1a5434e4b
Danilo F. Silva <danilofes@gmail.com>
2019-03-13 22:33:02 -0300

    Filter refactoring types from the oracle

--- refdiff-evaluation/src/test/java/refdiff/evaluation/icse/IcseDataset.java
+++ refdiff-evaluation/src/test/java/refdiff/evaluation/icse/IcseDataset.java
@@ -2,7 +2,10 @@ package refdiff.evaluation.icse;
 
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EnumSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,10 +15,15 @@ import refdiff.evaluation.AbstractDataset;
 import refdiff.evaluation.RefactoringDescriptionParser;
 import refdiff.evaluation.RefactoringRelationship;
 import refdiff.evaluation.RefactoringSet;
+import refdiff.evaluation.RefactoringType;
 
 public class IcseDataset extends AbstractDataset {
 	
 	protected final List<RefactoringSet> rMinerRefactorings = new ArrayList<>();
+	
+	protected final List<String> ignore = Arrays.asList("Change Package", "Move Attribute", "Pull Up Attribute", "Push Down Attribute");
+	
+	public static EnumSet<RefactoringType> refactoringTypes = EnumSet.complementOf(EnumSet.of(RefactoringType.PULL_UP_ATTRIBUTE, RefactoringType.PUSH_DOWN_ATTRIBUTE, RefactoringType.MOVE_ATTRIBUTE));
 	
 	public IcseDataset() {
 		RefactoringDescriptionParser parser = new RefactoringDescriptionParser();
@@ -28,6 +36,10 @@ public class IcseDataset extends AbstractDataset {
 			IcseCommit[] commits = reader.readValue(new FileReader("data/icse/data.json"));
 			int ignoredCount = 0;
 			int addedCount = 0;
+			int tpCount = 0;
+			int tpCount2 = 0;
+			int fpCount = 0;
+			int fpCount2 = 0;
 			int refCount = 0;
 			for (IcseCommit commit : commits) {
 				if (commit.ignore) {
@@ -43,30 +55,41 @@ public class IcseDataset extends AbstractDataset {
 				RefactoringSet rsRMiner = new RefactoringSet(repoUrl, commit.sha1);
 				RefactoringSet rsNotExpected = new RefactoringSet(repoUrl, commit.sha1);
 				for (IcseRefactoring refactoring : commit.refactorings) {
-					if (refactoring.type.equals("Change Package")) {
+					if (ignore.contains(refactoring.type)) {
 						ignoredCount++;
 						continue;
 					} else {
 						addedCount++;
 					}
-					List<RefactoringRelationship> refs = parser.parse(refactoring.description);
+					List<RefactoringRelationship> refs = parser.parse(refactoring.description).stream()
+						.filter(r -> refactoringTypes.contains(r.getRefactoringType()))
+						.collect(Collectors.toList());
+					
 					refCount += refs.size();
 
 					if (refactoring.validation.equals("TP") || refactoring.validation.equals("CTP")) {
 						rs.add(refs);
+						tpCount += refs.size();
 					} else if (refactoring.validation.equals("FP")) {
 						rsNotExpected.add(refs);
+						fpCount += refs.size();
 					}
 					if (refactoring.detectionTools.contains("RefactoringMiner")) {
 						rsRMiner.add(refs);
 					}
 				}
+				tpCount2 += rs.getRefactorings().size();
+				fpCount2 += rsNotExpected.getRefactorings().size();
 				add(rs, rsNotExpected);
 				rMinerRefactorings.add(rsRMiner);
 			}
-			//System.out.println("Ignored: " + ignoredCount);
-			//System.out.println("Added: " + addedCount);
-			//System.out.println("Added refs: " + refCount);
+//			System.out.println("Ignored: " + ignoredCount);
+//			System.out.println("Added: " + addedCount);
+//			System.out.println("Added refs: " + refCount);
+//			System.out.println("TP: " + tpCount);
+//			System.out.println("FP: " + fpCount);
+//			System.out.println("TP2: " + tpCount2);
+//			System.out.println("FP2: " + fpCount2);
 		} catch(Exception e) {
 			throw new RuntimeException(e);
 		}
