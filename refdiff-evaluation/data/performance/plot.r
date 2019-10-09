b61ab5814ea4d2815960f916f52b8e9c616fa04e
Danilo Ferreira e Silva <danilo.ferreira@almg.gov.br>
2019-10-09 10:57:10 -0300

    Plot execution time

--- refdiff-evaluation/data/performance/plot.r
+++ refdiff-evaluation/data/performance/plot.r
@@ -1,5 +1,5 @@
 library(ggplot2)
-library(plyr)
+library(dplyr)
 
 # disable scientific notation
 options(scipen=999)
@@ -16,6 +16,32 @@ plot1=ggplot(df, aes(x=tool, y=time)) + scale_y_log10() + ylab("Time (ms)") + xl
 
 ggsave("plot1.pdf", plot=plot1, width=4, height=4, units="in", dpi=300)
 
+sum(filter(df, tool == "RefDiff")$time)
+median(filter(df, tool == "RefDiff")$time)
+mean(filter(df, tool == "RefDiff")$time)
+max(filter(df, tool == "RefDiff")$time)
+sum(filter(df, tool == "RMiner")$time)
+median(filter(df, tool == "RMiner")$time)
+mean(filter(df, tool == "RMiner")$time)
+max(filter(df, tool == "RMiner")$time)
+
+
+#> sum(filter(df, tool == "RefDiff")$time)
+#[1] 394632
+#> median(filter(df, tool == "RefDiff")$time)
+#[1] 157
+#> mean(filter(df, tool == "RefDiff")$time)
+#[1] 368.815
+#> max(filter(df, tool == "RefDiff")$time)
+#[1] 10500
+#> sum(filter(df, tool == "RMiner")$time)
+#[1] 2188286
+#> median(filter(df, tool == "RMiner")$time)
+#[1] 109
+#> mean(filter(df, tool == "RMiner")$time)
+#[1] 2045.127
+#> max(filter(df, tool == "RMiner")$time)
+#[1] 85576
 
 #dfRefdiff=read.table("refdiff.txt", header=TRUE)
 #dfRminer=read.table("rminer.txt", header=TRUE)
