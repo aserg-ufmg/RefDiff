1498afabb0aeb97ee8c26c87f980131f97e08030
Danilo F. Silva <danilofes@gmail.com>
2019-10-17 17:03:51 -0300

    Add refdiff-viewer subproject

---
+++ refdiff-viewer/src/main/java/refdiff/viewer/RefDiffViewer.java
@@ -0,0 +1,13 @@
+package refdiff.viewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefDiffViewer {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(RefDiffViewer.class, args);
+	}
+	
+}
